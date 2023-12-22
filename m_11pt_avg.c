2d77a470f1eed62eacdfc97d8f2c4e4c76cbe03d
soboroff <ian.soboroff@nist.gov>
2023-12-21 20:35:36 -0500

    Add rounding to m_11pt_avg

--- m_11pt_avg.c
+++ m_11pt_avg.c
@@ -5,6 +5,7 @@
    research, non-commercial purposes. 
 */
 
+#include <math.h>
 #include "common.h"
 #include "sysfunc.h"
 #include "trec_eval.h"
@@ -69,13 +70,15 @@ te_calc_11ptavg(const EPI * epi, const REL_INFO * rel_info,
         return (UNDEF);
 
     /* translate percentage of rels as given in the measure params, to
-       an actual cutoff number of docs.  Note addition of 0.9 
-       means the default 11 percentages should have same cutoffs as
-       historical MAP implementations (eg, old trec_eval) */
+       an actual cutoff number of docs.  In v9 and prior, this was done
+       by adding 0.9 and casting to an integral type, which is not
+       platform-safe. Changing to use rounding is more consistent
+       between platforms, but may break equivalence to "historical
+       MAP implementations (eg, old trec_eval)" */
     if (NULL == (cutoffs = Malloc(tm->meas_params->num_params, long)))
          return (UNDEF);
     for (i = 0; i < tm->meas_params->num_params; i++)
-        cutoffs[i] = (long) (cutoff_percents[i] * rr.num_rel + 0.9);
+        cutoffs[i] = lround(cutoff_percents[i] * rr.num_rel);
 
     current_cut = tm->meas_params->num_params - 1;
     while (current_cut >= 0 && cutoffs[current_cut] > rr.num_rel_ret)
